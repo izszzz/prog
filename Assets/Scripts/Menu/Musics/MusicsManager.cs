23ae26d870bc0970ae817c9a57796b0d06d8528c
izszzz <izszzz@yahoo.co.jp>
2022-01-02 14:42:01 +0900

    fix

--- Assets/Scripts/Menu/Musics/MusicsManager.cs
+++ Assets/Scripts/Menu/Musics/MusicsManager.cs
@@ -1,17 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MusicsManager : MenuManager
-{
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+﻿public class MusicsManager : MenuManager
+{   
+    public override void SetPanel(ScreenManager go, int id){
+        go.musicId = id;
     }
 }
